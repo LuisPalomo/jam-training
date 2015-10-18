9605008883ac5b95eb1246783f000dcd01512683
LuisPalomo <luispalomosiv@gmail.com>
2015-10-18 20:31:39 +0200

    Objetivo y Enemigos
    Añadido el objetivo (JohnConnor) y los enemigos (T-1000) que se mueven
    automáticamente hacia él. Añadido también un Spawn de enemigos cada 3
    segundos.

--- TerminatorDefend/Assets/SCRIPTS/Enemy.cs
+++ TerminatorDefend/Assets/SCRIPTS/Enemy.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
     public int health = 100;
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // Navigate to objective
+        GameObject objective = GameObject.Find("JohnConnor");
+        if (objective)
+            GetComponent<NavMeshAgent>().destination = objective.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (health<=0)
         {
             Dead();
@@ -23,5 +33,13 @@ public class Enemy : MonoBehaviour {
         Destroy(gameObject);
     }
 
-
+    void OnTriggerEnter(Collider co)
+    {
+        // If objective then deal Damage
+        if (co.name == "JohnConnor")
+        {
+            co.GetComponentInChildren<Health>().decrease();
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ TerminatorDefend/Assets/SCRIPTS/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+    // The enemy that should be spawned
+    public GameObject enemyPrefab;
+
+    // Spawn Delay in seconds
+    public float interval = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("SpawnNext", interval, interval);
+    }
+
+    void SpawnNext()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+}
